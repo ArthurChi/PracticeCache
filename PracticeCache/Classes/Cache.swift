09b1ee578d6549aabc29a311ee480ccab44a574a
ArthurChi <arthurvassilychi@gmail.com>
2018-09-29 20:35:37 +0800

    抽象key类型

--- PracticeCache/Classes/Cache.swift
+++ PracticeCache/Classes/Cache.swift
@@ -9,19 +9,21 @@ import Foundation
 
 protocol CacheStandard {
     associatedtype ValueType: Codable
-    func containsObject(key: String) -> Bool
-    func query(key: String) -> ValueType?
-    func save(value: ValueType, for key: String)
-    func remove(key: String)
+    associatedtype Key: Hashable
+    func containsObject(key: Key) -> Bool
+    func query(key: Key) -> ValueType?
+    func save(value: ValueType, for key: Key)
+    func remove(key: Key)
     func removeAll()
 }
 
 protocol CacheAsyncStandard {
     associatedtype ValueType: Codable
-    func containsObject(key: String, _ result: @escaping ((_ key: String, _ contain: Bool) -> Void))
-    func query(key: String, _ result: @escaping ((_ key: String, _ value: ValueType?) -> Void))
-    func save(value: ValueType, for key: String, _ result: @escaping (()->Void))
-    func remove(key: String, _ result: @escaping ((_ key: String) -> Void))
+    associatedtype Key: Hashable
+    func containsObject(key: Key, _ result: @escaping ((_ key: Key, _ contain: Bool) -> Void))
+    func query(key: Key, _ result: @escaping ((_ key: Key, _ value: ValueType?) -> Void))
+    func save(value: ValueType, for key: Key, _ result: @escaping (()->Void))
+    func remove(key: Key, _ result: @escaping ((_ key: Key) -> Void))
     func removeAll(_ result: @escaping (()->Void))
 }
 
@@ -46,8 +48,10 @@ final class Mutex: Lock {
     }
 }
 
-struct Cache<T, M: CacheStandard, D: CacheStandard & CacheAsyncStandard> where M.ValueType == T, D.ValueType == T {
-    typealias ValueType = T
+struct Cache<K, V, M: CacheStandard, D: CacheStandard & CacheAsyncStandard> where M.ValueType == V, D.ValueType == V, M.Key == K, D.Key == K {
+    
+    typealias Key = K
+    typealias ValueType = V
     
     private var memoryCache: M
     private var diskCache: D
@@ -59,33 +63,32 @@ struct Cache<T, M: CacheStandard, D: CacheStandard & CacheAsyncStandard> where M
 }
 
 extension Cache: CacheStandard {
-    
-    func containsObject(key: String) -> Bool {
+    func containsObject(key: Key) -> Bool {
         return memoryCache.containsObject(key: key) || diskCache.containsObject(key: key)
     }
-    
-    func query(key: String) -> T? {
-        var value: T? = memoryCache.query(key: key)
+
+    func query(key: Key) -> ValueType? {
+        var value: ValueType? = memoryCache.query(key: key)
         if value == nil {
             value = diskCache.query(key: key)
             if let value = value {
                 memoryCache.save(value: value, for: key)
             }
         }
-        
+
         return value
     }
-    
-    func save(value: T, for key: String) {
+
+    func save(value: ValueType, for key: Key) {
         memoryCache.save(value: value, for: key)
         diskCache.save(value: value, for: key)
     }
-    
-    func remove(key: String) {
+
+    func remove(key: Key) {
         memoryCache.remove(key: key)
         diskCache.remove(key: key)
     }
-    
+
     func removeAll() {
         memoryCache.removeAll()
         diskCache.removeAll()
@@ -93,7 +96,7 @@ extension Cache: CacheStandard {
 }
 
 extension Cache: CacheAsyncStandard {
-    func containsObject(key: String, _ result: @escaping ((String, Bool) -> Void)) {
+    func containsObject(key: Key, _ result: @escaping ((Key, Bool) -> Void)) {
         if memoryCache.containsObject(key: key) {
             DispatchQueue.global().async {
                 result(key, true)
@@ -102,9 +105,9 @@ extension Cache: CacheAsyncStandard {
             diskCache.containsObject(key: key, result)
         }
     }
-    
-    func query(key: String, _ result: @escaping ((String, T?) -> Void)) {
-        if let value: T = memoryCache.query(key: key) {
+
+    func query(key: Key, _ result: @escaping ((Key, ValueType?) -> Void)) {
+        if let value: ValueType = memoryCache.query(key: key) {
             DispatchQueue.global().async {
                 result(key, value)
             }
@@ -112,17 +115,17 @@ extension Cache: CacheAsyncStandard {
             diskCache.query(key: key, result)
         }
     }
-    
-    func save(value: T, for key: String, _ result: @escaping (() -> Void)) {
+
+    func save(value: ValueType, for key: Key, _ result: @escaping (() -> Void)) {
         memoryCache.save(value: value, for: key)
         diskCache.save(value: value, for: key, result)
     }
-    
-    func remove(key: String, _ result: @escaping ((String) -> Void)) {
+
+    func remove(key: Key, _ result: @escaping ((Key) -> Void)) {
         memoryCache.remove(key: key)
         diskCache.remove(key: key, result)
     }
-    
+
     func removeAll(_ result: @escaping (() -> Void)) {
         memoryCache.removeAll()
         diskCache.removeAll(result)
