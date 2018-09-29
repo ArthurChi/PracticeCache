1b7eca8a67047fe5dfe098234a9e31b234d2e253
ArthurChi <arthurvassilychi@gmail.com>
2018-09-29 20:38:36 +0800

    类型建立

--- PracticeCache/Classes/DiskCache.swift
+++ PracticeCache/Classes/DiskCache.swift
@@ -10,3 +10,57 @@ import Foundation
 protocol DiskCacheable: CacheStandard, CacheAsyncStandard {
     init(path: URL)
 }
+
+struct DiskCache<T: Codable>: DiskCacheable {
+    typealias ValueType = T
+    
+    private var path: URL
+    
+    init(path: URL) {
+        self.path = path
+    }
+}
+
+extension DiskCache {
+    func containsObject(key: String) -> Bool {
+        return true
+    }
+    
+    func query(key: String) -> T? {
+        return nil
+    }
+    
+    func save(value: T, for key: String) {
+        
+    }
+    
+    func remove(key: String) {
+        
+    }
+    
+    func removeAll() {
+        
+    }
+}
+
+extension DiskCache {
+    func containsObject(key: String, _ result: @escaping ((String, Bool) -> Void)) {
+        
+    }
+    
+    func query(key: String, _ result: @escaping ((String, T?) -> Void)) {
+        
+    }
+    
+    func save(value: T, for key: String, _ result: @escaping (() -> Void)) {
+        
+    }
+    
+    func remove(key: String, _ result: @escaping ((String) -> Void)) {
+        
+    }
+    
+    func removeAll(_ result: @escaping (() -> Void)) {
+        
+    }
+}
--- PracticeCache/Classes/MemoryCache.swift
+++ PracticeCache/Classes/MemoryCache.swift
@@ -7,9 +7,72 @@
 
 import Foundation
 
-protocol MemoryCacheable: CacheStandard {
+protocol MemoryCacheable: CacheStandard { }
+
+struct MemoryLinkNode<V: Codable> {
     
 }
 
+struct MemoryLink<T: Codable> {
+    
+    var head: MemoryLinkNode<T>?
+    var trail: MemoryLinkNode<T>?
+    private(set) var totalCost: Int = 0
+    private(set) var totalCount: Int = 0
+    
+    func insertNodeToHead(_ node: MemoryLinkNode<T>) {
+        
+    }
+    
+    func bringNodeToHead(_ node: MemoryLinkNode<T>) {
+        
+    }
+    
+    func removeNode(_ node: MemoryLinkNode<T>) {
+        
+    }
+    
+    func removeTrailNode() {
+        
+    }
+    
+    func removeAll() {
+        
+    }
+}
+
+struct MemoryCache<T: Codable> {
+    typealias ValueType = T
+    
+    private let lock: Lock = Mutex()
+    private var dict = [String : T]()
+    private var link: MemoryLink<T>
+    
+    
+    
+    
+}
+
+extension MemoryCache: MemoryCacheable {
+    func containsObject(key: String) -> Bool {
+        return dict.contains(where: { $0.key == key })
+    }
+    
+    func query(key: String) -> T? {
+        return dict[key]
+    }
+    
+    func save(value: T, for key: String) {
+        
+    }
+    
+    func remove(key: String) {
+        
+    }
+    
+    func removeAll() {
+        
+    }
+    
     
 }
