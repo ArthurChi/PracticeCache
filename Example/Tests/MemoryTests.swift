fc3752ea0dc588828fb2ce789e70d0af3752d348
ArthurChi <arthurvassilychi@gmail.com>
2018-09-29 20:41:13 +0800

    添加内存缓存单元测试

---
+++ Example/Tests/MemoryTests.swift
@@ -0,0 +1,32 @@
+//
+//  MemoryTests.swift
+//  PracticeCache_Tests
+//
+//  Created by Vassily on 2018/9/29.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import XCTest
+
+class MemoryTests: XCTestCase {
+
+    override func setUp() {
+        
+    }
+
+    override func tearDown() {
+        
+    }
+    
+    func test_query() {
+        
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
