9a1b6cf6cb7e0d9ab2fd572957348878c30ae01b
VassilyChi <cjfire@sina.cn>
2018-10-25 19:10:51 +0800

    面相协议修改完成

--- MemoryCacheLink.swift
+++ MemoryCacheLink.swift
@@ -7,84 +7,130 @@
 
 import Foundation
 
-public class Node<Value> {
+public protocol NodeStandard: class, CustomStringConvertible {
+    associatedtype Key where Key: Hashable
+    associatedtype Value
     
-    var value: Value
-    var next: Node?
+    var key: Key { get }
+    var value: Value { get }
+    var next: Self? { get }
+    init(key: Key, value: Value, next: Self?)
+}
+
+extension NodeStandard {
+    public var description: String {
+        guard let next = next else { return "\(value)" }
+        return "\(value) -> \(next)"
+    }
     
-    init(value: Value, next: Node? = nil) {
+    init(key: Key, value: Value, next: Self? = nil) {
+        self.init(key: key, value: value, next: next)
+    }
+}
+
+protocol LinkedNodeListIndexStandard: Comparable {
+    associatedtype Node: NodeStandard
+    var node: Node? { get }
+    init(node: Node?)
+}
+
+extension LinkedNodeListIndexStandard {
+    public static func == (lhs: Self, rhs: Self) -> Bool {
+        switch (lhs.node, rhs.node) {
+        case let (left?, right?):
+            return left.next === right.next
+        case (nil, nil):
+            return true
+        default:
+            return false
+        }
+    }
+    
+    public static func < (lhs: Self, rhs: Self) -> Bool {
+        guard lhs != rhs else { return false }
+        let nodes = sequence(first: lhs.node, next: { $0?.next })
+        return nodes.contains(where: { $0 === rhs.node })
+    }
+}
+
+public protocol LinkedNodeListStandard: Collection where Index: LinkedNodeListIndexStandard {
+    associatedtype Node where Self.Node == Index.Node
+    associatedtype Key where Self.Key == Node.Key
+    associatedtype Value where Self.Value == Node.Value
+    
+    var head: Node? { get }
+    var trail: Node? { get }
+    subscript(key: Key) -> Node? { get set }
+}
+
+final public class LinkNode<K: Hashable, V>: NodeStandard {
+    public typealias Key = K
+    public typealias Value = V
+    
+    public var key: Key
+    public var value: Value
+    public var next: LinkNode?
+    
+    public init(key: Key, value: Value, next: LinkNode? = nil) {
+        self.key = key
         self.value = value
         self.next = next
     }
 }
 
-extension Node: CustomStringConvertible {
-    public var description: String {
-        guard let next = next else { return "\(value)" }
-        return "\(value) -> \(next)"
+public struct LinkedNodeListIndex<K: Hashable, V>: LinkedNodeListIndexStandard {
+    var node: LinkNode<K, V>?
+    init(node: LinkNode<K, V>?) {
+        self.node = node
     }
 }
 
-public struct LinkedList<Value> {
+public struct LinkedList<K: Hashable, V>: LinkedNodeListStandard {
     
-    var head: Node<Value>?
-    var trail: Node<Value>?
+    public typealias Key = K
+    public typealias Value = V
+    public typealias Node = LinkNode<K, V>
+    public typealias Index = LinkedNodeListIndex<K, V>
+    
+    public var head: Node?
+    public var trail: Node?
+    
+    private var dictContainer = Dictionary<Key, Node>()
     
     public init() {}
     
     // head-first insertion
-    public mutating func push(_ value: Value) {
-        head = Node(value: value, next: head)
+    public mutating func push(_ value: Value, for key: Key) {
+        head = Node(key: key, value: value, next: head)
         if trail == nil {
             trail = head
         }
     }
-}
-
-extension LinkedList: CustomStringConvertible {
-    public var description: String {
-        guard let head = head else { return "empty" }
-        return "\(head)"
+    
+    public subscript(key: K) -> LinkNode<K, V>? {
+        get {
+            return head!
+        }
+        set {
+            
+        }
     }
 }
 
 extension LinkedList: Collection {
-    public typealias Element = Value
-    
-    public struct Index: Comparable {
-        var node: Node<Value>?
-        
-        public static func == (lhs: Index, rhs: Index) -> Bool {
-            switch (lhs.node, rhs.node) {
-            case let (left?, right?):
-                return left.next === right.next
-            case (nil, nil):
-                return true
-            default:
-                return false
-            }
-        }
-        
-        public static func < (lhs: LinkedList<Value>.Index, rhs: LinkedList<Value>.Index) -> Bool {
-            guard lhs != rhs else { return false }
-            let nodes = sequence(first: lhs.node, next: { $0?.next })
-            return nodes.contains(where: { $0 === rhs.node })
-        }
-    }
-    
-    public var startIndex: LinkedList<Value>.Index {
+    public var startIndex: Index {
         return Index(node: head)
     }
     
-    public var endIndex: LinkedList<Value>.Index {
+    public var endIndex: Index {
         return Index(node: trail?.next)
     }
     
-    public func index(after i: LinkedList<Value>.Index) -> LinkedList<Value>.Index {
+    public func index(after i: Index) -> Index {
         return Index(node: i.node?.next)
     }
     
-    public subscript(position: LinkedList<Value>.Index) -> Value {
-        return position.node!.value
+    public subscript(position: Index) -> Node {
+        return position.node!
     }
 }
