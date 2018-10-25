ff65b2de7159d8b2b0f801024c40e488174ce134
VassilyChi <cjfire@sina.cn>
2018-10-25 14:27:26 +0800

    添加文件

---
+++ Example/Tests/MemoryCacheLinkTests.swift
@@ -0,0 +1,15 @@
+//
+//  MemoryCacheLinkTests.swift
+//  PracticeCache_Tests
+//
+//  Created by Vassily on 2018/10/22.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import XCTest
+import PracticeCache
+
+class MemoryCacheLinkTests: XCTestCase {
+
+    
+}
---
+++ MemoryCacheLink.swift
@@ -0,0 +1,8 @@
+//
+//  MemoryCacheLink.swift
+//  PracticeCache
+//
+//  Created by Vassily on 2018/10/22.
+//
+
+import Foundation
